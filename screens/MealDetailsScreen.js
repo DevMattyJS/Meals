9c82f6bb149662845c25fb6874001218f7d909fe
DevMattyJS <matej.sliacky@protonmail.com>
2023-02-25 12:08:07 +0100

    Styling added to the MealDetailsScreen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,14 +1,60 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
+
+import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
 
 function MealDetailsScreen({ route }) {
   const mealId = route.params.mealId;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View>
-      <Text>MealDetails Screen</Text>
-      <Text>Meal ID: {mealId}</Text>
-    </View>
+    <ScrollView style={styles.rootContainer}>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 export default MealDetailsScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  listOuterContainer: {
+    alignItems: "center",
+  },
+  listContainer: {
+    width: "80%",
+  },
+});
